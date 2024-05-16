c811283182cc30fa0c65e62d976f8222d2f1e960
sockmage <cockmage@icloud.com>
2024-05-17 00:09:36 +0300

    Создание панели управления

--- app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/AppDrawer.kt
@@ -1,2 +1,44 @@
 package com.topic2.android.notes.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.NotesTheme
+
+@Composable
+private fun AppDrawerHeader() {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Image(
+            imageVector = Icons.Filled.Menu,
+            contentDescription = "Drawer Header Icon",
+            colorFilter = ColorFilter
+                .tint(MaterialTheme.colors.onSurface),
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = "Заметки",
+            modifier = Modifier
+                .align(alignment = Alignment.CenterVertically)
+        )
+    }
+}
+
+
+@Preview
+@Composable
+fun AppDrawerHeaderPreview() {
+    NotesTheme {
+        AppDrawerHeader()
+    }
+}
