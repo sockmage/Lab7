7765f7932cdca70ce7f9843b2aa4eca03b7a1ada
sockmage <cockmage@icloud.com>
2024-05-19 16:23:21 +0300

    Улучшение удобства использования NoteColor

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -12,12 +13,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun NoteColor() {
+fun NoteColor(
+    modifier: Modifier = Modifier,
+    color: Color,
+    size: Dp,
+    padding: Dp = 0.dp,
+    border: Dp
+) {
     Box(
         modifier = Modifier
+            .padding(4.dp)
             .size(40.dp)
             .clip(CircleShape)
             .background(Color.Red)
@@ -35,5 +44,5 @@ fun NoteColor() {
 @Preview
 @Composable
 fun NoteColorPreview() {
-    NoteColor()
+    NoteColor(color = Color.Red, size = 40.dp, padding = 4.dp, border = 2.dp)
 }
