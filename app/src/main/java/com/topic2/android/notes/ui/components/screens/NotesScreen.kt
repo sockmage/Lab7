8939f22eb7255ca99f5df4c4ed6dd2070b0ccae9
sockmage <cockmage@icloud.com>
2024-05-19 19:14:24 +0300

    Отображение заметок на экране заметок

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
@@ -1,9 +1,38 @@
 package com.topic2.android.notes.ui.components.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import com.topic2.android.notes.domain.model.NoteModel
+import com.topic2.android.notes.ui.components.Note
+import com.topic2.android.notes.ui.components.TopAppBar
 import com.topic2.android.notes.viewmodel.MainViewModel
 
 @Composable
 fun NotesScreen(viewModel: MainViewModel) {
+    val notes: List<NoteModel> by viewModel
+        .notesNotInTrash
+        .observeAsState(listOf())
 
+    Column {
+        TopAppBar(
+            title = "Заметки",
+            icon = Icons.Filled.List,
+            onIconClick = {}
+        )
+        LazyColumn {
+            items(count = notes.size) { noteIndex ->
+                val note = notes[noteIndex]
+                Note(
+                    note = note,
+                    onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) },
+                    onNoteClick = { viewModel.onNoteClick(it) }
+                )
+            }
+        }
+    }
 }
