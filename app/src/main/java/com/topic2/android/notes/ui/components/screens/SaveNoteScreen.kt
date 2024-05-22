b2046a9634da89671b81448fc1a6beada0e0fcdb
sockmage <cockmage@icloud.com>
2024-05-22 23:02:13 +0300

    Подтверждение действия удаления

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/SaveNoteScreen.kt
@@ -1,11 +1,14 @@
 package com.topic2.android.notes.ui.components.screens
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.BottomDrawer
 import androidx.compose.material.BottomDrawerState
 import androidx.compose.material.BottomDrawerValue
@@ -16,6 +19,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
@@ -25,15 +29,20 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.rememberBottomDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.topic2.android.notes.R
 import com.topic2.android.notes.domain.model.ColorModel
 import com.topic2.android.notes.domain.model.NEW_NOTE_ID
@@ -56,6 +65,10 @@ fun SaveNoteScreen(viewModel: MainViewModel) {
     val bottomDrawerState: BottomDrawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
     val coroutineScope = rememberCoroutineScope()
 
+    val moveNoteToTrashDialogShownState: MutableState<Boolean> = rememberSaveable {
+        mutableStateOf(false)
+    }
+
     Scaffold(topBar = {
         val isEditingMode: Boolean = noteEntry.id != NEW_NOTE_ID
         SaveNoteTopAppBar(
@@ -70,7 +83,7 @@ fun SaveNoteScreen(viewModel: MainViewModel) {
                 coroutineScope.launch { bottomDrawerState.open() }
             },
             onDeleteNoteClick = {
-                viewModel.moveNoteToTrash(noteEntry)
+                moveNoteToTrashDialogShownState.value = true
             }
         )
     },
@@ -94,6 +107,35 @@ fun SaveNoteScreen(viewModel: MainViewModel) {
                     )
                 }
             )
+        if (moveNoteToTrashDialogShownState.value) {
+            AlertDialog(
+                onDismissRequest = {
+                    moveNoteToTrashDialogShownState.value = false
+                },
+                title = {
+                    Text("Move note to the trash?")
+                },
+                text = {
+                    Text(
+                        "Are you sure you want to" + "move this note to the trash?"
+                    )
+                },
+                confirmButton = {
+                    TextButton(onClick = {
+                        viewModel.moveNoteToTrash(noteEntry)
+                    }) {
+                        Text("Confirm")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = {
+                        moveNoteToTrashDialogShownState.value = false
+                    }) {
+                        Text("Dismiss")
+                    }
+                }
+            )
+            }
         }
     )
 }
@@ -257,6 +299,73 @@ private fun PickedColor(color: ColorModel) {
 }
 
 
+@Composable
+private fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+){
+    Column (
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text(
+            text = "Color picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        )  {
+            items(
+                colors.size
+            ) {
+                    itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorItem(
+    color: ColorModel,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                onClick = {
+                    onColorSelect(color)
+                }
+            )
+    ) {
+        NoteColor(
+            modifier = Modifier
+                .padding(10.dp), color = Color.fromHex(color.hex), size = 80.dp,
+            border = 2.dp
+        )
+        Text(
+            text = color.name,
+            fontSize = 22.sp,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun ColorItemPreview() {
+    ColorItem(ColorModel.DEFAULT) {}
+}
+
+
 @Preview
 @Composable
 fun SaveNoteTopAppBarPreview() {
@@ -267,6 +376,19 @@ fun SaveNoteTopAppBarPreview() {
         onOpenColorPickerClick = {},
         onDeleteNoteClick = {}
     )
+}
+
+
+@Preview
+@Composable
+fun ColorPickerPreview() {
+    ColorPicker(
+        colors = listOf(
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT,
+            ColorModel.DEFAULT
+        )
+    ) { }
 }
 
 
