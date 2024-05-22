6e2f826097f8c388cfcab93e212d1eec7ebfc650
sockmage <cockmage@icloud.com>
2024-05-22 21:21:52 +0300

    Добавление контейнера Scaffold

--- app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/screens/NotesScreen.kt
@@ -2,6 +2,7 @@ package com.topic2.android.notes.ui.components.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
@@ -19,18 +20,23 @@ fun NotesScreen(viewModel: MainViewModel) {
         .notesNotInTrash
         .observeAsState(listOf())
 
-    Column {
+    Scaffold(topBar = {
         TopAppBar(
-            title = "Заметки",
-            icon = Icons.Filled.List,
-            onIconClick = {}
+            title = "Notes",
+            icon = Icons.Filled.List, onIconClick = {}
         )
-        NotesList(
-            notes = notes,
-            onNoteCheckedChange = { viewModel.onNoteCheckedChange(it) },
-            onNoteClick = { viewModel.onNoteClick(it) }
-        )
-    }
+    },
+        content = { it ->
+            if (notes.isNotEmpty()) {
+                NotesList(
+                    notes = notes, onNoteCheckedChange = {
+                        viewModel.onNoteCheckedChange(it)
+                    },
+                    onNoteClick = { viewModel.onNoteClick(it) }
+                )
+            }
+        }
+    )
 }
 
 
